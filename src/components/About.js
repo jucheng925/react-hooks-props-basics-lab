cadbee7d0e966af699441fd1711aac7549ba3c90
Julie C <jucheng925@gmail.com>
2023-09-03 12:28:50 -0400

    fixed the conditional rendering

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,20 @@ import React from "react";
 import Links from "./Links"
 
 function About(props) {
+  function checkBio() {
+    if (!("bio" in props)) {
+      return null
+    }
+    else if (props.bio === "") {
+      return null
+    }
+    else return <p>{props.bio}</p>
+  }
 
   return (
     <div id="about">
       <h2>About Me</h2>
-      {(props.bio === null) ? null : <p>{props.bio}</p>}
+      {checkBio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links github={props.links.github} linkedin ={props.links.linkedin}/>
     </div>
